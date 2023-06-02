383d9b207046ed00a2eca0032e8949bb6a9df254
Marcos <marcos.smz76@gmail.com>
2023-06-02 14:17:22 -0300

    1.1
    Adição de anotações e maior segurança contra sql injection

--- login.php
+++ login.php
@@ -5,17 +5,27 @@ header("Access-Control-Allow-Origin: http://localhost:3000");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header("Access-Control-Allow-Headers: Content-Type");
 
+// Recebendo o POST feito pelo front
 $postdata = json_decode(file_get_contents('php://input'), true);
 
+// Verificando se há algo no POST
 if(isset($postdata) && !empty($postdata)){
-     
-    $email = $postdata["email"];
-    $senha = $postdata["senha"];
 
+    // Selecionando o conteúdo
+    $jsonemail = $postdata["email"];
+    $jsonsenha = $postdata["senha"];
+
+    // Checando por sql injection
+    $email = mysqli_real_escape_string($conexao, $jsonemail);
+    $senha = mysqli_real_escape_string($conexao, $jsonsenha);
+
+    // Comando de busca no mysql
     $sql = "SELECT id, nome, nivel FROM usuarios WHERE email = '{$email}' AND senha = SHA1('{$senha}') LIMIT 1";
-
+    
+    // Query de busca no banco de dados
     $query =  mysqli_query($conexao, $sql);
 
+    // Checando se as informações batem
     if (mysqli_num_rows($query) != 1) {
     
         http_response_code(422); 
@@ -24,6 +34,7 @@ if(isset($postdata) && !empty($postdata)){
         
         http_response_code(200);
 
+        // Selecionando informações que serão úteis para o frontend
         $resultado = mysqli_fetch_assoc($query);
         if (!isset($_SESSION)) session_start();
     
@@ -53,17 +64,18 @@ if(isset($postdata) && !empty($postdata)){
         $tipouser = 'servicosgerais';
     };
     
+    // O que vamos retornar para o front
     $returnData = [
         'id' => $_SESSION['UsuarioID'],
         'nome' => $_SESSION['UsuarioNome'],
         'tipo1' => $_SESSION['UsuarioNivel'],
         'tipo2' => $tipouser
     ];
-
-        echo json_encode($returnData);
+        // Enviando em formato json que suporta utf-8
+        echo json_encode($returnData, JSON_UNESCAPED_UNICODE);
     };
          
 };
 
-
+// Fechando a conexão
 mysqli_close($conexao);
